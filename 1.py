e7cb4aeabeefa96d17112f6b51e35cd3ffc873e1
donxan <donxan@gmail.com>
2023-06-20 17:35:11 +0800

    updagte auto request times

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 import requests as req
-import json,sys,time
+import json,sys,time,random
 #先注册azure应用,确保应用有以下权限:
 #files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
 #mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用.
+#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
 
 
 
@@ -28,8 +28,6 @@ def gettoken(refresh_token):
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
-    with open(path, 'w+') as f:
-        f.write(refresh_token)
     return access_token
 def main():
     fo = open(path, "r+")
@@ -42,6 +40,9 @@ def main():
     'Authorization':access_token,
     'Content-Type':'application/json'
     }
+    print('此次运行开始时间为 :', localtime)
+    with open('./time.log','a') as fd:
+        fd.write(localtime + "begin request")
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
@@ -52,7 +53,7 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
             print('3调用成功'+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
+        if req.get(r'https://graph.microsoft.com/v1.0/users',headers=headers).status_code == 200:
             num1+=1
             print('4调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
@@ -61,7 +62,7 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
             print('6调用成功'+str(num1)+'次')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
             print('7调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
@@ -76,9 +77,10 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('10调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
         pass
-for _ in range(3):
+for _ in range(6):
     main()
+    for i in range(random.randint(600, 1200),0,-1):
+        time.sleep(1)
